26cfde85a2143fadda41435c41339bff04cf9226
Dominic Bennett <dominic.john.bennett@gmail.com>
2018-10-21 16:48:39 +0200

    better file sending and retrieving

--- R/functions.R
+++ R/functions.R
@@ -4,14 +4,18 @@ base_function <- function(cmd, ...) {
   args <- outsider::.args_parse(...)
   # wd is determined either by -wd or the location of the input file
   if ('-wd' %in% args) {
-    wd <- args[which(args == '-wd') + 1]
+    wd_i <- which(args == '-wd')
+    wd <- args[wd_i + 1]
+    # if wd is specified, then drop from args
+    args <- args[-1 * c(wd_i, wd_i + 1)]
   } else {
     pattern <- paste0(.Platform$file.sep, basename(args[1]))
     wd <- sub(pattern = pattern, replacement = '', x = args[1])
   }
-  files_to_send <- outsider::.which_args_are_filepaths(args)
+  files_to_send <- outsider::.which_args_are_filepaths(args, wd)
   outsider::.run(pkgnm = pkgnm, files_to_send = files_to_send, dest = wd,
-                 'python2.7', paste0('/PyRate/', cmd), args)
+                 'python2.7', paste0('/PyRate/', cmd),
+                 outsider::.to_basename(args))
 }
 
 #' @name PyRate
