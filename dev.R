b1997bff51ebf0be84fd07eea623347b69240251
Dominic Bennett <dominic.john.bennett@gmail.com>
2018-11-02 21:09:49 +0100

    renamed package

--- dev.R
+++ dev.R
@@ -1,3 +1,4 @@
 files_to_send <- '/home/dom/Desktop/pyrate_test/Rhinocerotidae_PyRate.py'
 args <- '/Users/djb208/Desktop/pyrate_test/Rhinocerotidae_PyRate.py'
 cmd = 'PyRate.py'
+pkgnm <- outsider::.repo_to_pkgnm('dombennett/om..pyrate..2.0')
